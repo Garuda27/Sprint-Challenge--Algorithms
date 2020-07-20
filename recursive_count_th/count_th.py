7f98a7f0a377b462be330b28ce9cf1b5205cd99d
Preston <pres.10.nap@gmail.com>
2020-07-19 21:54:10 -0400

    MVP

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,32 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-    # TBC
+    count = 0
+
+    def inner(word):
+        nonlocal count
+        if len(word) == 0:
+            return 0
+        if len(word) == 1:
+            return 0
+        if len(word) > 1:
+
+            arr = list(word)
+            word1 = ''
+            print(word)
+            if arr[0] == 't' and arr[1] == 'h':
+                count += 1
+                word1 = word[2:]
+            elif arr[0] == 't' and arr[1] == 't':
+                word1 = word[1:]
+            elif arr[0] == 't' and arr[1] != 'h':
+                word1 = word[2:]
+            elif arr[0] != 't':
+                word1 = word[1:]
+            print(word1)
+            print(count)
+        return inner(word1)
+    inner(word)
+    return count
     
-    pass
+    print(count_th("tthhhtthht"))
